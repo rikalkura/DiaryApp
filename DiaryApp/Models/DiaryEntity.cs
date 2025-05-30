79f4ac7ceb2db5e0872d3bed5bf2bcaba2e7142f
rikalkura <mad060605@gmail.com>
2025-05-30 19:57:30 +0300

    MSSQL connected, migration succeeded

---
+++ DiaryApp/Models/DiaryEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace DiaryApp.Models;
+
+public class DiaryEntity : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public DiaryEntity()
+    {
+        Title = string.Empty;
+        Description = string.Empty;
+        CreatedAt = DateTime.Now;
+    }
+}
